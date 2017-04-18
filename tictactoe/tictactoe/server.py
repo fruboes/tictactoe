b2cb3a0ca4db8e2e7cc4a6d4b18599bfd7b358ca
Tomasz Fruboes <tfruboes@fuw.edu.pl>
2017-04-18 14:10:54 +0200

    serve files

---
+++ tictactoe/tictactoe/server.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash, send_from_directory
+
+app = Flask(__name__) # create the application instance :)
+app.config.from_object(__name__)
+
+@app.route('/')
+def index():
+    return send_from_directory("templates", "index.html")
+
+@app.route('/<string:name>')
+def static_files(name):
+    print "XXX", name
+    return send_from_directory("templates", name)
